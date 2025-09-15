96448876e486cbd259cec79ccedcc1ac16ecdd37
Rtificer <Rtificer@users.noreply.github.com>
2025-09-15 17:36:50 +0000

    deploy: 9340dd8ae05589765e57cdf56631904f4ac63d9e

--- search/all_11.js
+++ search/all_11.js
@@ -1,9 +1,10 @@
 var searchData=
 [
   ['sensor_20bias_20estimation_20and_20observability_0',['Sensor Bias Estimation and Observability',['../EKF__sensor__fusion__controller__cleaned_8hpp.html#bias_estimation',1,'']]],
-  ['settings_5f_1',['settings_',['../classrov__controllers_1_1AxisToCommandController.html#a757e6ebd52b154730effa1e62dd3e645',1,'rov_controllers::AxisToCommandController']]],
-  ['solve_5fthruster_5fqp_2',['solve_thruster_qp',['../axis__to__command__controller_8hpp.html#a38687d7993a68fa0042b964b60a6c1a0',1,'axis_to_command_controller.hpp']]],
-  ['state_20definition_3',['State Definition',['../EKF__sensor__fusion__controller__cleaned_8hpp.html#state_vector',1,'']]],
-  ['state_5f_4',['state_',['../classrov__control_1_1PWMInterface.html#a2f207b516495acedd3650ad285e29aa0',1,'rov_control::PWMInterface::state_'],['../classrov__controllers_1_1PidController.html#a7f27e6f4e04d20bf01bf7e565dcbd490',1,'rov_controllers::PidController::state_']]],
-  ['state_5finterface_5fconfiguration_5',['state_interface_configuration',['../classrov__controllers_1_1AxisToCommandController.html#a14a2407d58c87e2c05843a3beed98f43',1,'rov_controllers::AxisToCommandController::state_interface_configuration()'],['../classrov__controllers_1_1PidController.html#acaadeafef93e2c0d74bab3474b0136b5',1,'rov_controllers::PidController::state_interface_configuration()']]]
+  ['sequence_1',['Update Sequence',['../EKF__sensor__fusion__controller__cleaned_8hpp.html#update_sequence',1,'']]],
+  ['settings_5f_2',['settings_',['../classrov__controllers_1_1AxisToCommandController.html#a757e6ebd52b154730effa1e62dd3e645',1,'rov_controllers::AxisToCommandController']]],
+  ['solve_5fthruster_5fqp_3',['solve_thruster_qp',['../axis__to__command__controller_8hpp.html#a38687d7993a68fa0042b964b60a6c1a0',1,'axis_to_command_controller.hpp']]],
+  ['state_20definition_4',['State Definition',['../EKF__sensor__fusion__controller__cleaned_8hpp.html#state_vector',1,'']]],
+  ['state_5f_5',['state_',['../classrov__control_1_1PWMInterface.html#a2f207b516495acedd3650ad285e29aa0',1,'rov_control::PWMInterface::state_'],['../classrov__controllers_1_1PidController.html#a7f27e6f4e04d20bf01bf7e565dcbd490',1,'rov_controllers::PidController::state_']]],
+  ['state_5finterface_5fconfiguration_6',['state_interface_configuration',['../classrov__controllers_1_1AxisToCommandController.html#a14a2407d58c87e2c05843a3beed98f43',1,'rov_controllers::AxisToCommandController::state_interface_configuration()'],['../classrov__controllers_1_1PidController.html#acaadeafef93e2c0d74bab3474b0136b5',1,'rov_controllers::PidController::state_interface_configuration()']]]
 ];
--- search/all_13.js
+++ search/all_13.js
@@ -1,8 +1,9 @@
 var searchData=
 [
   ['update_0',['update',['../EKF__sensor__fusion__controller_8hpp.html#ekf_update',1,'EKF Measurement Update'],['../classrov__controllers_1_1PidController.html#ab8fc8949920b632ba47fcd086b77c021',1,'rov_controllers::PidController::update()']]],
-  ['update_5fand_5fwrite_5fcommands_1',['update_and_write_commands',['../classrov__controllers_1_1AxisToCommandController.html#a484718069a50ddae3d2d7eb79b533cbe',1,'rov_controllers::AxisToCommandController']]],
-  ['update_5freference_5ffrom_5fsubscribers_2',['update_reference_from_subscribers',['../classrov__controllers_1_1AxisToCommandController.html#a19c2c7aa050d07645cb2670f31be1a49',1,'rov_controllers::AxisToCommandController']]],
-  ['usbl_20position_20measurement_20model_20and_20range_20dependent_20noise_20covariance_3',['USBL Position Measurement Model and Range-Dependent Noise Covariance',['../EKF__sensor__fusion__controller_8hpp.html#usb_cov',1,'']]],
-  ['usbl_20range_20dependent_20noise_20modeling_4',['USBL Range-Dependent Noise Modeling',['../EKF__sensor__fusion__controller__cleaned_8hpp.html#usbl_covariance',1,'']]]
+  ['update_20sequence_1',['Update Sequence',['../EKF__sensor__fusion__controller__cleaned_8hpp.html#update_sequence',1,'']]],
+  ['update_5fand_5fwrite_5fcommands_2',['update_and_write_commands',['../classrov__controllers_1_1AxisToCommandController.html#a484718069a50ddae3d2d7eb79b533cbe',1,'rov_controllers::AxisToCommandController']]],
+  ['update_5freference_5ffrom_5fsubscribers_3',['update_reference_from_subscribers',['../classrov__controllers_1_1AxisToCommandController.html#a19c2c7aa050d07645cb2670f31be1a49',1,'rov_controllers::AxisToCommandController']]],
+  ['usbl_20position_20measurement_20model_20and_20range_20dependent_20noise_20covariance_4',['USBL Position Measurement Model and Range-Dependent Noise Covariance',['../EKF__sensor__fusion__controller_8hpp.html#usb_cov',1,'']]],
+  ['usbl_20range_20dependent_20noise_20modeling_5',['USBL Range-Dependent Noise Modeling',['../EKF__sensor__fusion__controller__cleaned_8hpp.html#usbl_covariance',1,'']]]
 ];
